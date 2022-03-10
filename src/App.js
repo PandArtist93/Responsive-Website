1843d4b0de5014cf12baf83da55e33a033b4deb3
Marielena <maizaga01@gmail.com>
2022-03-10 14:10:00 -0500

    project settings 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
+/* import {Article, Brand, CTA, Feature, Navbar} from './components' */
 
 const App = () => {
   return (
     <div className="App">
-      <h1>
-        GPT-3
-      </h1>
-      
+      <h1>GPT-3</h1>      
     </div>
   );
 }
