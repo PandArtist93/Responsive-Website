c4bc2964e53429a67908ed62f7cb79762927ff9f
Marielena <maizaga01@gmail.com>
2022-03-11 00:55:24 -0500

    build component header and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-/* import {Article, Brand, CTA, Feature, Navbar} from './components' */
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers'
+import { CTA, Brand, Navbar } from './components'
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>GPT-3</h1>      
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div> 
+      <Brand /> 
+      <WhatGPT3 />
+      <Feature />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
