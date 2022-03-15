748450ed348391372e3335133ca1bef1298e0eb3
Marielena <maizaga01@gmail.com>
2022-03-15 00:37:46 -0500

    build section features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './features.css'
 
-const Features = () => {
+const Features = ({title, text}) => {
   return (
-    <div>
-        Features
+    <div className='gpt3__features-container__feature'>
+      <div className='gpt3__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container_feature-text'>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
